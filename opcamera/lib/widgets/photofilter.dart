493bd225229d9f64ae10578f3a2005eb34a454b2
goyalgitesh <goyalg1017@gmail.com>
2021-08-04 11:58:30 +0530

    Photofilter widget 1st commit

---
+++ opcamera/lib/widgets/photofilter.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:path/path.dart';
+import 'package:photofilters/photofilters.dart';
+import 'package:image/image.dart' as imageLib;
+import 'package:image_picker/image_picker.dart';
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+
+class PhotoFilters extends StatefulWidget {
+  const PhotoFilters({ Key? key }) : super(key: key);
+
+  @override
+  _PhotoFiltersState createState() => _PhotoFiltersState();
+}
+
+class _PhotoFiltersState extends State<PhotoFilters> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
+
+
+
